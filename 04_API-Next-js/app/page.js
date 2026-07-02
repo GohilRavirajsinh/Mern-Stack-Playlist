1de5fcccd17fd1c3909689970a2fe3b5b3d3a169
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2026-07-02 16:17:23 +0530

    Next.js Lerning

---
+++ 04_API-Next-js/app/page.js
@@ -0,0 +1,26 @@
+'use client'
+// import Image from "next/image"; // this is for Image component that not define in example
+
+export default function Home() {
+  const handleClick = async () => {
+    let data = {
+      name: 'Gamer',
+      role: 'Frager'
+    }
+    let a = await fetch('/api/add', {
+      method: 'POST', headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+    let res = await a.json()
+    console.log(res);
+  }
+
+  return (
+    <div>
+      <h1 className="text-xl font-bold">Next.js api routes demo</h1>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+}
